81e6cbe0040539707c2a852560c998bcbca180a0
AbdulSaboorArif <abdulsaboorarif73@gmail.com>
2025-02-02 03:49:41 +0500

    Commit 1 website

--- final-project/src/app/page.tsx
+++ final-project/src/app/page.tsx
@@ -1,7 +1,19 @@
 
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import HomeProduct from "@/components/HomeProduct" 
+import BeautifullRoom from '@/components/Beautifullroom'
+import Footer from '@/components/Fotter'
 
 export default function Home() {
   return (
-    <div>This is the Final Ecmmerce Website</div>
-  );
+    <main className="min-h-screen">
+          
+      <Header />
+      <Hero />
+      <HomeProduct/>
+      <BeautifullRoom/>
+      <Footer/>
+    </main>
+  )
 }
